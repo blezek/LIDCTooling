ef4a989b4316ab1f86ca28e81fb9e44b6a69ea5e
Daniel Blezek <daniel.blezek@gmail.com>
2016-05-30 06:24:56 -0500

    Change virtualenv location

--- devops/cluster-install.sh
+++ devops/cluster-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-source sc-env/bin/activate
+source venv/bin/activate
 cluster_meta="$(starcluster listclusters $1 2>&1)"
 # MASTER=$(echo "${cluster_meta}" | grep "master running" | sed -e 's/.*master.*\(ec2-.*com\)/\1/g')
 
