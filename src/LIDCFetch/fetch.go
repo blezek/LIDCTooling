0b2f7037974e0d5b55b9961a88f2a3a9f3be40e3
Daniel Blezek <daniel.blezek@gmail.com>
2016-10-03 21:24:36 -0500

    Fixed broken logging

--- src/LIDCFetch/fetch.go
+++ src/LIDCFetch/fetch.go
@@ -46,37 +46,37 @@ func fetchImage(context *cli.Context) {
 	req, err := prepareRequest("/query/getImage", params)
 	resp, err := client.Do(req)
 	if err != nil {
-		logger.Error("Error fetching collections")
+		logger.Errorf("Error fetching collections")
 	}
 	defer resp.Body.Close()
 
-	logger.Debug("Response: %+v", resp)
+	logger.Debugf("Response: %+v", resp)
 	if resp.StatusCode != http.StatusOK {
-		logger.Error("Got bad status '%v' from server", resp.Status)
+		logger.Errorf("Got bad status '%v' from server", resp.Status)
 		os.Exit(1)
 	}
 
 	// Do we extract somewhere?
 	if context.String("extract") == "" {
-		logger.Debug("Saving to %v", OutputZip)
+		logger.Debugf("Saving to %v", OutputZip)
 		f, err := os.Create(OutputZip)
 		if err != nil {
-			logger.Error("Error opening %v for writing: %v", OutputZip, err.Error())
+			logger.Errorf("Error opening %v for writing: %v", OutputZip, err.Error())
 			return
 		}
 		defer f.Close()
 		io.Copy(f, resp.Body)
 	} else {
 		outputDir := context.String("extract")
-		logger.Debug("Extracting to %v", outputDir)
+		logger.Debugf("Extracting to %v", outputDir)
 		err := os.MkdirAll(outputDir, os.ModeDir|os.ModePerm)
 		if err != nil {
-			logger.Error("Error creating directory %v: %v", outputDir, err.Error())
+			logger.Errorf("Error creating directory %v: %v", outputDir, err.Error())
 			return
 		}
 		fid, err := ioutil.TempFile(os.TempDir(), "LIDCFetch")
 		if err != nil {
-			logger.Error("Error creating temp file: %v", outputDir, err.Error())
+			logger.Errorf("Error creating temp file: %v", outputDir, err.Error())
 			return
 		}
 		defer os.Remove(fid.Name())
@@ -85,47 +85,47 @@ func fetchImage(context *cli.Context) {
 		// https://github.com/gosuri/uiprogress
 		_, err = io.Copy(fid, resp.Body)
 		if err != nil {
-			logger.Error("Error downloading zip: %v", err.Error())
+			logger.Errorf("Error downloading zip: %v", err.Error())
 			return
 		}
 		fid.Close()
-		logger.Debug("Finished saving to %v", fid.Name())
+		logger.Debugf("Finished saving to %v", fid.Name())
 		// Unzip in the specified location
 		z, err := zip.OpenReader(fid.Name())
 		if err != nil {
-			logger.Error("Error opening zip archive: %v", err.Error())
+			logger.Errorf("Error opening zip archive: %v", err.Error())
 			return
 		}
 		defer z.Close()
 		// Loop over the files
 		for _, f := range z.File {
 			extractedFilename := path.Join(outputDir, f.Name)
-			logger.Info("Extracting %v Saving %v", f.Name, extractedFilename)
+			logger.Infof("Extracting %v Saving %v", f.Name, extractedFilename)
 			if strings.HasSuffix(f.Name, "/") {
 				// Create the directory
-				logger.Debug("Making directory %v", extractedFilename)
+				logger.Debugf("Making directory %v", extractedFilename)
 				err = os.MkdirAll(extractedFilename, os.ModeDir|os.ModePerm)
 				if err != nil {
-					logger.Error("Error creating directory %v: %v", extractedFilename, err.Error())
+					logger.Errorf("Error creating directory %v: %v", extractedFilename, err.Error())
 					return
 				}
 				continue
 			}
 			efid, err := os.Create(extractedFilename)
 			if err != nil {
-				logger.Error("Error opening output file %v: %v", extractedFilename, err.Error())
+				logger.Errorf("Error opening output file %v: %v", extractedFilename, err.Error())
 				return
 			}
 			defer efid.Close()
 			zfid, err := f.Open()
 			if err != nil {
-				logger.Error("Error opening zip file %v: %v", f.Name, err.Error())
+				logger.Errorf("Error opening zip file %v: %v", f.Name, err.Error())
 				return
 			}
 			defer zfid.Close()
 			_, err = io.Copy(efid, zfid)
 			if err != nil {
-				logger.Error("Error extracting zip file %v to %v: %v", f.Name, extractedFilename, err.Error())
+				logger.Errorf("Error extracting zip file %v to %v: %v", f.Name, extractedFilename, err.Error())
 				return
 			}
 		}
